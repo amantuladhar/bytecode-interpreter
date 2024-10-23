0a30d5991a3f0c7a48af490e69788243bb6eb287
Aman Tuladhar <atuladhar.aman@gmail.com>
2024-10-23 15:55:23 -0500

    add simple files

---
+++ in_clang/chunk.c
@@ -0,0 +1,27 @@
+
+
+#include <stdlib.h>
+
+#include "memory.h"
+#include "chunk.h"
+
+void initChunk(Chunk *chunk) {
+    chunk->count = 0;
+    chunk->capacity = 0;
+    chunk->code = NULL;
+}
+
+void freeChunk(Chunk *chunk) {
+    FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    initChunk(chunk);
+}
+
+void writeChunk(Chunk* chunk, uint8_t byte) {
+    if (chunk->count >= chunk->capacity) {
+        int old_capacity = chunk->capacity;
+        chunk->capacity = GROW_CAPACITY(old_capacity);
+        chunk->code = GROW_ARRAY(uint8_t, chunk->code, old_capacity, chunk->capacity);
+    }
+    chunk->code[chunk->count] = byte;
+    chunk->count++;
+}
--- in_clang/main.c
+++ in_clang/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+#include "chunk.h"
+
+int main(int argc, const char* argv[]) {
+
+    printf("-- PROGRAM STARTED --\n");
+    printf("------------------\n\n");
+    
+    Chunk chunk;
+
+    initChunk(&chunk);
+
+    writeChunk(&chunk, OP_RETURN);
+
+    freeChunk(&chunk);
+
+    printf("\n\n------------------\n");
+    printf("-- PROGRAM ENDED--\n");
     return 0;
 }
